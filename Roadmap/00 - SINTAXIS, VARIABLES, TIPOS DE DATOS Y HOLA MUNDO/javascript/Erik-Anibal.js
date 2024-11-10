be6ee94a0ee0ca05058b06e0899e3fafaed5d4a0
Erik-Anibal <eom.tmds@gmail.com>
2024-11-10 15:14:41 -0600

    listo!

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Erik-Anibal.js
@@ -0,0 +1,23 @@
+/* https://www.javascript.com/ */
+
+// Comentario de una linea/
+/* Comentario de varias
+lineas*/
+
+var nombre;
+const apellido = "Ortiz";
+
+let radio = 10;
+const pi = 3.1416;
+
+let crustaceo = "Don Cangrejo";
+let codo = true;
+let caja_chica = 1000000.50;
+let caja_fuerte = 10000000000000
+let simbolo = Symbol("$dinero");
+let noDefinido = undefined;
+let estatus = null;
+let sinValorInicial;
+sinValorInicial = 10;
+
+console.log("Hola, JavaScript!");
---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Erik-Anibal.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+
+# Comentario de una línea
+"""
+Comentario de varias
+líneas
+"""
+
+# Declaración de variables
+nombre = "Erik"
+APELLIDO = "Ortiz"
+
+radio = 10
+pi = 3.1416
+
+crustaceo = "Don Cangrejo"
+codo = True
+caja_chica = 1000000.50
+caja_fuerte = 10000000000000
+no_definido = None
+estatus = None
+sin_valor_inicial = None
+sin_valor_inicial = 10
+
+# Imprimir en consola
+print("Hola, Python!")
